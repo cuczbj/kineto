ab9fbb89a1d0a039248a845f6e40f7eca4301d6e
cuczbj <56664384+cuczbj@users.noreply.github.com>
2024-04-21 22:08:11 +0800

    Update ActivityProfilerController.cpp

--- libkineto/src/ActivityProfilerController.cpp
+++ libkineto/src/ActivityProfilerController.cpp
@@ -5,7 +5,7 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+//控制活动跟踪、调度执行等
 #include "ActivityProfilerController.h"
 
 #include <chrono>
