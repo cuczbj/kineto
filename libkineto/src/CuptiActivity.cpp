e917322cafc42ac5b37c77aea96e15e8cbacc0a0
cuczbj <56664384+cuczbj@users.noreply.github.com>
2024-04-22 10:15:35 +0800

    Update CuptiActivity.cpp

--- libkineto/src/CuptiActivity.cpp
+++ libkineto/src/CuptiActivity.cpp
@@ -5,7 +5,7 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+//CUPTI的活动的处理函数
 #include "CuptiActivity.h"
 
 #include <fmt/format.h>
